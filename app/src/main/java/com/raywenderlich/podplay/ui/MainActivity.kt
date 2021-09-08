7728cee0d93c21ef1e862681e0a058987b97ffc2
HAIND <HAIND haind@hqsoft.com.vn>
2021-09-08 14:12:31 +0700

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/podplay/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.raywenderlich.podplay.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.raywenderlich.podplay.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
